a2da91fe1d1401e7e1176d1a4a7024f8c98fb4dc
JoshuaConde9 <joshuaconde0909@gmail.com>
2025-11-06 20:36:19 -0500

    Resumes Activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._companyName = "Teleperformance";
+        job1._jobTitle = "Customer Expert";
+        job1._startYear = 2024;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._companyName = "Concentrix";
+        job2._jobTitle = "Sales Representative";
+        job2._startYear = 2019;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Joshua Conde";
+        myResume._jobs = [job1, job2];
+        myResume.DisplayResume();
     }
 }
